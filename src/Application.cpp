db268e86d9572156b8d4860c0eeb1441d5156c7a
Nigel Saurino <nigelsaurino@gmail.com>
2024-09-25 17:38:32 -0500

    implementing QuickFIX application interface for the client side

--- src/Application.cpp
+++ src/Application.cpp
@@ -4,15 +4,31 @@
 
 #include "quickfix/fix42/ExecutionReport.h" // Not sure I need this include
 
-void Application::onLogon( const FIX::SessionID& sessionID ) {}
+void Application::onLogon( const FIX::SessionID& sessionID ) {
+  std::cout << std::endl << "Logon: " << sessionID << std::endl;
+}
 
-void Application::onLogout( const FIX::SessionID& sessionID ) {}
+void Application::onLogout( const FIX::SessionID& sessionID ) {
+  std::cout << std::endl << "Logoff: " << sessionID << std::endl;
+}
 
 void Application::fromApp( const FIX::Message& message,const FIX::SessionID& sessionID )
                     EXCEPT( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue,
                      FIX::UnsupportedMessageType )
 {
   crack( message, sessionID );
+  std::cout << std::endl << "IN: " << message << std::endl;
+}
+
+void Application::toApp( FIX::Message& message, const FIX::SessionID& sessionID)
+        EXCEPT( FIX::DoNotSend ){
+  try{
+    FIX::PossDupFlag possDupFlag;
+    message.getHeader().getField(possDupFlag);
+    if(possDupFlag){throw FIX::DoNotSend();}
+  }catch(FIX::FieldNotFound&){}
+
+  std::cout << std::endl << "OUT: " << message << std::endl;
 }
 
 void Application::onMessage( const FIX42::MarketDataRequest& message, const FIX::SessionID& )
@@ -38,7 +54,70 @@ void Application::onMessage( const FIX42::MarketDataRequest& message, const FIX:
   }
 }
 
-void Application::onMessage( const FIX43::MarketDataRequest& message, const FIX::SessionID& )
-{
-    std::cout << message.toXML() << std::endl;
+void Application::run(){
+  while(true){
+    try{
+      char query_action = queryAction();
+
+      switch (query_action)
+      {
+      case '1':
+        /* code */
+        // new order
+        queryEnterOrder();
+        break;
+      
+      case '2':
+        // cancel order
+        queryCancelOrder();
+        break;
+      
+      case '3':
+        // modify order
+        queryReplaceOrder();
+        break;
+
+      case '4':
+        // market data request
+        queryMarketDataRequest();
+        break;
+
+      case '5':
+        break;
+
+      default:
+        break;
+      }
+    }catch(std::exception& e){
+      std::cout << "Error sending message: " << e.what() << std::endl;
+    }
+  }
+}
+
+void Application::queryEnterOrder(){
+
+}
+
+void Application::queryCancelOrder(){
+
+}
+
+void Application::queryReplaceOrder(){
+
+}
+
+void Application::queryMarketDataRequest(){
+
+}
+
+char Application::queryAction(){
+    char action;
+    std::cout << "1) New Order,\n2) Cancel Order,\n3) Modify Order,\n4)Market Data Request\n5) Quit,\nAction: ";
+    std::cin >> action;
+
+    switch(action){
+      case '1': case '2': case '3': case '4': case '5': break; 
+      default: throw std::exception();
+    }
+    return action;
 }
